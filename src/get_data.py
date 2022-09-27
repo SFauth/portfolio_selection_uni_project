f5452499d1ca6516aebbad367820974b403d1df4
SFauth <72087109+SFauth@users.noreply.github.com>
2022-09-27 22:26:25 +0900

    Delete get_data.py

--- src/get_data.py
+++
@@ -1,8 +0,0 @@
-#%% Get DSWS access
-import DatastreamDSWS as DSWS
-
-ds = DSWS.Datastream(username='stefan.fauth@student.uni-tuebingen.de', password="abc")
-
-# %%
-ds.get_data(tickers='VOD', fields='P', kind=0)
-# %%
